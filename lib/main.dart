22cecb89a43ac400f47c79945e535eb83f885163
Tony Bark <35226681+tonytins@users.noreply.github.com>
2021-05-08 19:00:08 -0400

    macOS and Linux support

--- lib/main.dart
+++ lib/main.dart
@@ -9,15 +9,6 @@ class BullsEyeApp extends StatelessWidget {
     return MaterialApp(
       title: 'BullsEye',
       theme: ThemeData(
-        // This is the theme of your application.
-        //
-        // Try running your application with "flutter run". You'll see the
-        // application has a blue toolbar. Then, without quitting the app, try
-        // changing the primarySwatch below to Colors.green and then invoke
-        // "hot reload" (press "r" in the console where you ran "flutter run",
-        // or simply save your changes to "hot reload" in a Flutter IDE).
-        // Notice that the counter didn't reset back to zero; the application
-        // is not restarted.
         primarySwatch: Colors.blue,
       ),
       home: GamePage(title: 'BullsEye'),
@@ -26,8 +17,8 @@ class BullsEyeApp extends StatelessWidget {
 }
 
 class GamePage extends StatefulWidget {
-  GamePage({Key key, this.title}) : super(key: key);
-  final String title;
+  GamePage({Key? key, this.title}) : super(key: key);
+  final String? title;
 
   @override
   _GamePageState createState() => _GamePageState();
--- test/widget_test.dart
+++ test/widget_test.dart
@@ -11,12 +11,8 @@ import 'package:flutter_test/flutter_test.dart';
 import 'package:bullseye/main.dart';
 
 void main() {
-  testWidgets('Smoke test', (WidgetTester tester) async {
+  testWidgets('Counter increments smoke test', (WidgetTester tester) async {
     // Build our app and trigger a frame.
     await tester.pumpWidget(BullsEyeApp());
-
-    // Tap the '+' icon and trigger a frame.
-    await tester.tap(find.text("Hit me!"));
-    await tester.pump();
   });
 }
